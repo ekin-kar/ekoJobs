f893796f602537df726e3bec4533e59db5b624f0
Ekin Kar <kar_ekin@outlook.com>
2024-04-21 20:44:47 +0300

    added navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,22 @@
+import Link from "next/link";
+import React from "react";
+import logo from "@/assets/logo.png";
+import Image from "next/image";
+import { Button } from "./ui/button";
+
+export default function Navbar() {
+  return (
+    <header className="shadow-sm">
+      <nav className="m-auto flex max-w-5xl items-center justify-between px-3 py-5">
+        <Link href="/" className="flex items-center gap-3">
+          <Image src={logo} width={40} height={40} alt="eko jobs logo" />
+          <span className="text-xl font-bold tracking-tight">Eko Jobs</span>
+        </Link>
+        <Button asChild>
+          <Link href="/jobs/new">Post a Job</Link>
+        </Button>
+        <div>Element 2</div>
+      </nav>
+    </header>
+  );
+}
