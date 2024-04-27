fdfadf4b4529d6e35215f894069776e29f9b42f7
Ekin Kar <kar_ekin@outlook.com>
2024-04-28 00:40:10 +0300

    added pagination

--- src/components/JobResults.tsx
+++ src/components/JobResults.tsx
@@ -1,17 +1,25 @@
-import React from "react";
-import JoblistItem from "./JoblistItem";
 import prisma from "@/lib/prisma";
+import { cn } from "@/lib/utils";
 import { JobFilterValues } from "@/lib/validation";
 import { Prisma } from "@prisma/client";
+import { ArrowLeft, ArrowRight } from "lucide-react";
 import Link from "next/link";
+import JoblistItem from "./JoblistItem";
 
 interface JobResultsProps {
   filterValues: JobFilterValues;
+  page?: number;
 }
 
 export default async function JobResults({
-  filterValues: { q, type, location, remote },
+  filterValues,
+  page = 1,
 }: JobResultsProps) {
+  const { q, type, location, remote } = filterValues;
+
+  const jobsPerPage = 6;
+  const skip = (page - 1) * jobsPerPage;
+
   const searchString = q
     ?.split(" ")
     .filter((word) => word.length > 0)
@@ -39,10 +47,16 @@ export default async function JobResults({
     ],
   };
 
-  const jobs = await prisma.job.findMany({
+  const jobsPromise = prisma.job.findMany({
     where,
     orderBy: { createdAt: "desc" },
+    take: jobsPerPage,
+    skip,
   });
+
+  const countPromise = prisma.job.count({ where });
+
+  const [jobs, totalResults] = await Promise.all([jobsPromise, countPromise]);
 
   return (
     <div className="grow space-y-4">
@@ -52,8 +66,69 @@ export default async function JobResults({
         </Link>
       ))}
       {jobs.length === 0 && (
-        <p className="m-auto text-center">No Jobs Found:(</p>
+        <p className="m-auto text-center">
+          No jobs found. Try adjusting your search filters.
+        </p>
       )}
+      {jobs.length > 0 && (
+        <Pagination
+          currentPage={page}
+          totalPages={Math.ceil(totalResults / jobsPerPage)}
+          filterValues={filterValues}
+        />
+      )}
+    </div>
+  );
+}
+
+interface PaginationProps {
+  currentPage: number;
+  totalPages: number;
+  filterValues: JobFilterValues;
+}
+
+function Pagination({
+  currentPage,
+  totalPages,
+  filterValues: { q, type, location, remote },
+}: PaginationProps) {
+  function generatePageLink(page: number) {
+    const searchParams = new URLSearchParams({
+      ...(q && { q }),
+      ...(type && { type }),
+      ...(location && { location }),
+      ...(remote && { remote: "true" }),
+      page: page.toString(),
+    });
+
+    return `/?${searchParams.toString()}`;
+  }
+
+  return (
+    <div className="flex justify-between">
+      <Link
+        href={generatePageLink(currentPage - 1)}
+        className={cn(
+          "flex items-center gap-2 font-semibold",
+          currentPage <= 1 && "invisible",
+        )}
+      >
+        <ArrowLeft size={16} />
+        Previous page
+      </Link>
+      <span className="font-semibold">
+        Page {currentPage} of {totalPages}
+      </span>
+      <Link
+        href={generatePageLink(currentPage + 1)}
+        className={cn(
+          "flex items-center gap-2 font-semibold",
+          currentPage >= totalPages && "invisible",
+        )}
+      >
+        Next page
+        <ArrowRight size={16} />
+      </Link>
     </div>
   );
 }
